4e788251663a8f675ea896936ab2a32ab73b3e06
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2024-02-11 06:13:47 -0800

    Update mainserver.js

--- mainserver.js
+++ mainserver.js
@@ -1,10 +1,7 @@
 const io = require('socket.io')(80);
 var servers = {}
 io.on('connection', (socket) => {
-if(socket.handshake.auth.id){
-    console.log("server girdi")
-    servers[socket.handshake.auth.id] = socket.id
-}
+if(socket.handshake.auth.id) servers[socket.handshake.auth.id] = socket.id
 socket.on("newclient",msg =>io.to(servers[msg.to]).emit("newclient",msg))
 socket.on("catch", msg=>io.to(servers[msg.id]).emit("catch",msg))
 socket.on("message", msg=>io.to(msg.to).emit("message",msg))
