0287983ea1b8b377e0c076febfbb2d62d77a521b
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-20 17:55:47 +0300

    Update user.js

--- Socketio/user.js
+++ Socketio/user.js
@@ -8,28 +8,28 @@ const io = socketio(url);
 var ltnidd = 0
 var da = Date.now()
 io.on('connect', () => {
-    console.log("connectted server",Date.now()-da)
+    console.log("connectted server", Date.now() - da)
     da = Date.now()
-    io.emit("catch",{id:id,socket:io.id})
-    io.on("startit",function(msg){
-            console.log("connectted proxy",Date.now()-da)
-        if(msg.socket != io.id) return;
+    io.emit("catch", { id: id, socket: io.id })
+    io.on("startit", function (msg) {
+        console.log("connectted proxy", Date.now() - da)
+        if (msg.socket != io.id) return;
         ltnidd = msg.data
-  net.createServer((sourceSocket) => {
-    var ltnid = ltnidd++;
-   io.emit("newclient",{
-        id:ltnid,
-        socket:io.id,
-        to:id
+        net.createServer((sourceSocket) => {
+            var ltnid = ltnidd++;
+            io.emit("newclient", {
+                id: ltnid,
+                socket: io.id,
+                to: id
+            })
+
+
+            io.on("message", function (msg) {
+                if (msg.id == ltnid) sourceSocket.write(msg.data)
+            })
+
+            sourceSocket.on('data', (data) => io.emit("_msg", { to: id, id: ltnid, data: data }));
+            sourceSocket.on('end', () => io.emit("logout", { to: id, socket: io.id }));
+        }).listen(port);
     })
-
-
-    io.on("message",function(msg) {
-        if(msg.id == ltnid) sourceSocket.write(msg.data)
-    })
-
-    sourceSocket.on('data', (data) =>  io.emit("_msg",{to:id,id:ltnid,data:data}));
-    sourceSocket.on('end', () => io.emit("logout",{to:id,socket:io.id}));
-  }).listen(port);
- })
 })
